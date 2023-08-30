994b4e11c1fefe666169fa5cc344bed7437140ea
rondido <rppr001@naver.com>
2023-08-30 22:37:25 +0900

    feature: component folder에서 storybook tailwindcss setup

--- component/src/stories/button/Button.stories.ts
+++ component/src/stories/button/Button.stories.ts
@@ -13,9 +13,6 @@ const meta = {
     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
     tags: ['autodocs'],
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -24,35 +21,6 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
     args: {
-        primary: true,
-        label: 'Button',
-    },
-};
-
-export const Secondary: Story = {
-    args: {
-        label: 'Button',
-    },
-};
-
-export const Large: Story = {
-    args: {
-        size: 'large',
-        label: 'Button',
-    },
-};
-
-export const Small: Story = {
-    args: {
-        size: 'small',
-        label: 'Button',
-    },
-};
-
-export const Warning: Story = {
-    args: {
-        primary: true,
-        label: 'Delete now',
-        backgroundColor: 'red',
+        label: 'label입니다',
     },
 };
