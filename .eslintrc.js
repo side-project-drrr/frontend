eb2e7051db0ef6a4d90ecbbfcc121e6b6a3f85cd
rondido <rppr001@naver.com>
2023-09-09 04:18:22 +0900

    root monorepo vitest 진행중

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
         'plugin:react-hooks/recommended',
         'plugin:prettier/recommended',
         'react-app',
+        'plugin:vitest-globals/recommended',
     ],
     //parser: '@typescript-eslint/parser',
     parserOptions: {
