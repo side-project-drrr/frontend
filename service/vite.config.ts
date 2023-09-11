797dd671329bbeae16ab75bae9b5c6463cec15aa
rondido <rppr001@naver.com>
2023-09-11 23:58:55 +0900

    vitest global 설정 중

--- service/vite.config.ts
+++ service/vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    test: {
+        environment: 'jsdom',
+    },
     plugins: [react()],
 });
