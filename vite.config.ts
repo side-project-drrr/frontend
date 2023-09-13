c9a2c323ff9fe277f6421799ebb56c101e4bcd8f
rondido <rppr001@naver.com>
2023-09-13 22:56:18 +0900

    vitest 글로벌 설정 중 pnp와 호환 안됨

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    test: {
-        globals: true,
-        environment: 'jsdom',
-    },
     plugins: [react()],
 });
