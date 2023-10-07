e8e1999247d9a9ec8727d24825d30d61b714bf5d
rondido <rppr001@naver.com>
2023-10-07 15:52:50 +0900

    feature: sidebar toggle 완료

--- admin/src/components/layouts/Layout.tsx
+++ admin/src/components/layouts/Layout.tsx
@@ -1,7 +1,9 @@
 import { Outlet, useNavigate } from 'react-router-dom';
 import Navbar from '@monorepo/component/src/stories/navbar/NavbarComponent';
+import { useState } from 'react';
 
 export default function Layout() {
+    const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
     const navigate = useNavigate();
     const handleCategoryMove = () => {
         navigate('/category');
@@ -11,9 +13,21 @@ export default function Layout() {
     };
     return (
         <div className="flex items-center w-screen">
-            <aside className="w-48">
-                <Navbar handleCategoryMove={handleCategoryMove} handleMainMove={handleMainMove} />
-            </aside>
+            {sidebarOpen ? (
+                <aside className="w-48">
+                    <Navbar
+                        handleCategoryMove={handleCategoryMove}
+                        handleMainMove={handleMainMove}
+                        setSidebarOpen={setSidebarOpen}
+                    />
+                </aside>
+            ) : (
+                <>
+                    <div className="z-50 flex flex-col justify-start h-screen w-10">
+                        <p onClick={() => setSidebarOpen(true)}>열기</p>
+                    </div>
+                </>
+            )}
             <section className="flex h-screen w-screen justify-center grow items-center">
                 <Outlet />
             </section>
--- component/src/stories/navbar/NavbarComponent.tsx
+++ component/src/stories/navbar/NavbarComponent.tsx
@@ -1,16 +1,25 @@
 import { Button } from '@nextui-org/react';
+import { Dispatch, SetStateAction } from 'react';
 
 interface Props {
     handleCategoryMove: () => void;
     handleMainMove: () => void;
+    setSidebarOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-export default function NavbarComponent({ handleCategoryMove, handleMainMove }: Props) {
+export default function NavbarComponent({
+    handleCategoryMove,
+    handleMainMove,
+    setSidebarOpen,
+}: Props) {
     return (
         <aside className="flex justify-center h-screen bg-white">
             <div className="flex flex-col h-screen p-3  shadow w-full">
                 <div className="w-full flex justify-center text-center mb-20 text-black">
-                    <h4 onClick={handleMainMove}>Logo(여기 클릭 시 Main이동)</h4>
+                    <h4 onClick={handleMainMove}>Logo</h4>
+                    <div className="relative" style={{ right: '-80px' }}>
+                        <p onClick={() => setSidebarOpen(false)}>닫기</p>
+                    </div>
                 </div>
                 <Button
                     className="border-none outline-none bg-transparent"
