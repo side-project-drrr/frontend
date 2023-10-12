dd3f45965e07b925e18b6b2a91deeda053ac89da
rondido <rppr001@naver.com>
2023-10-12 12:32:19 +0900

    feature: author filter search 완료

--- component/src/stories/table/selectData.ts
+++ component/src/stories/table/selectData.ts
@@ -1,9 +1,10 @@
 export const selectData = [
+    { label: '전체', value: '', description: '전체', id: '1' },
     {
         label: '블로그 작성자',
-        value: 'blog',
+        value: '블로그 작성자',
         description: '블로그 작성자',
-        id: '1',
+        id: '2',
     },
-    { label: '제목', value: 'title', description: '제목', id: '2' },
+    { label: '제목', value: '제목', description: '제목', id: '3' },
 ];
