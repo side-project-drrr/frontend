eeada7df0d08f958d9d1fbbf0d0397188b7c3058
rondido <rppr001@naver.com>
2023-11-06 16:23:00 +0900

    feature: service repo msw setting

--- service/src/main.tsx
+++ service/src/main.tsx
@@ -4,6 +4,11 @@ import App from './App.tsx';
 import './index.css';
 import { NextUIProvider } from '@nextui-org/react';
 import { BrowserRouter } from 'react-router-dom';
+import { worker } from './mocks/brower.ts';
+
+if (import.meta.env.MODE === 'development') {
+    worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
