4a3cccda8a2fe32ae0675038c2c6ee61b060a24a
rondido <rppr001@naver.com>
2023-11-06 16:12:03 +0900

    feature: admin repo  msw settings

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { worker } from './mocks/brower.ts';
+if (import.meta.env.MODE === 'development') {
+    worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
--- admin/src/pages/Main.tsx
+++ admin/src/pages/Main.tsx
@@ -1,3 +1,17 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 export default function Main() {
+    async function testFetch() {
+        try {
+            const res = await axios.get('/posts');
+            return res.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    useEffect(() => {
+        testFetch();
+    }, []);
     return <div>Main</div>;
 }
