f17e81e3da03fc2e4136f8fe4788003f456f0a2c
rondido <rppr001@naver.com>
2023-12-06 00:34:22 +0900

    storybook: ListBox 완성

--- component/src/stories/listbox/Listbox.stories.ts
+++ component/src/stories/listbox/Listbox.stories.ts
@@ -1,11 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import ListBox from './ListBox';
 
-import Listbox from './Listbox';
+const itmes = [
+    {
+        id: 1,
+        title: '우아한형제들 PM의 이야기- “배민 기획자의 일”',
+        content:
+            '우아한형제들 PM(Product Manager)는 어떻게 일할까? 실제 이야기를 담은 책 "배민 기획자의 일"  PM들과 함께 나눈 이야기를 담았습니다.',
+        bookmark: 1000,
+        views: 50000,
+    },
+];
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
     title: 'Example/Listbox',
-    component: Listbox,
+    component: ListBox,
     parameters: {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
         layout: 'centered',
@@ -13,7 +23,7 @@ const meta = {
     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
     tags: ['autodocs'],
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-} satisfies Meta<typeof Listbox>;
+} satisfies Meta<typeof ListBox>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -21,15 +31,6 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const List: Story = {
     args: {
-        items: [
-            {
-                id: 1,
-                title: '우아한형제들 PM의 이야기- “배민 기획자의 일”',
-                content:
-                    '우아한형제들 PM(Product Manager)는 어떻게 일할까? 실제 이야기를 담은 책 "배민 기획자의 일"  PM들과 함께 나눈 이야기를 담았습니다.',
-                bookmark: 100,
-                views: 5000,
-            },
-        ],
+        items: itmes,
     },
 };
--- component/src/stories/listbox/Listbox.tsx
+++ component/src/stories/listbox/Listbox.tsx
@@ -1,17 +1,23 @@
 import { Props } from './index';
-export default function Listbox({ items }: Props) {
+
+import { FaHeart } from 'react-icons/fa';
+
+export default function ListBox({ items }: Props) {
     return (
         <>
             {items.map(item => (
                 <div
                     key={item.id}
-                    className="flex flex-col flex-wrap text-black bg-white w-[650px] h-[155px] justify-center rounded-lg"
+                    className="flex flex-col flex-wrap text-black bg-white w-[650px] h-[155px] justify-center rounded-lg pl-6 gap-3"
                 >
                     <p className="text-base font-bold bold ">{item.title}</p>
-                    <p className="w-full text-xs">{item.content}</p>
-                    <div>
-                        <span className="text-xs ">{item.bookmark}</span>
-                        <span className="text-xs ">{item.views}</span>
+                    <p className="w-10/12 text-xs">{item.content}</p>
+                    <div className="flex justify-between w-2/12">
+                        <span className="flex items-center justify-around text-xs text-center">
+                            <FaHeart size={12} />
+                            {item.bookmark}
+                        </span>
+                        <span className="text-xs">{item.views}</span>
                     </div>
                 </div>
             ))}
