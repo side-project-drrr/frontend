9680b78af3d5b92e6ec59b7645c1f307c59d98f1
wjdtj9656 <wjdtj9656@naver.com>
2024-01-26 06:26:06 +0900

    refactor: axios 공통코드 사용

--- service/src/service/TopKeyword.ts
+++ service/src/service/TopKeyword.ts
@@ -4,3 +4,8 @@ export async function getTopkeyword() {
     const res = await HttpClient.get('/api/v1/top/categories/6');
     return res.data;
 }
+
+export async function getKeyword() {
+    const res = await HttpClient.get('/api/v1/categories');
+    return res.data;
+}
