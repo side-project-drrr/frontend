82baec5350094f760f184c17f74fafac368b298a
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-02-24 22:18:42 +0900

    fix: 모드 스타일 컨트로 mul로 통합

--- service/src/App.tsx
+++ service/src/App.tsx
@@ -9,31 +9,46 @@ import { ThemeProvider } from '@mui/material';
 import { darkModeState } from './recoil/atom/darkModeState';
 import { useRecoilValue } from 'recoil';
 import { createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { useMemo } from 'react';
+import { useDarkMode } from './ThemeContext/ThemeProvider';
 
 function App() {
-    const darkMode = useRecoilValue(darkModeState);
+    // const darkMode = useRecoilValue(darkModeState);
+    const { darkMode } = useDarkMode();
 
-    const lightTheme = createTheme({
-        palette: {
-            mode: 'light',
-            background: {
-                default: 'red', // 배경색 설정
+    const lightTheme = useMemo(() => 
+        createTheme({
+            palette: {
+                mode: 'light',
+                background: {
+                    default: '#ffffff', // 배경색 설정
+                },
+                text: {
+                    primary: '#2c2c2c'
+                }
             },
-        },
-    });
+        })
+    , [darkMode])
 
     // Dark Mode에서의 배경색
-    const darkTheme = createTheme({
-        palette: {
-            mode: 'dark',
-            background: {
-                default: 'red', // 배경색 설정
+    const darkTheme = useMemo(() => 
+        createTheme({
+            palette: {
+                mode: 'dark',
+                background: {
+                    default: '#2c2c2c', // 다크 모드의 주요 색상 설정
+                },
+                text: {
+                    primary: '#ffffff'
+                }
             },
-        },
-    });
+        })
+    , [darkMode])
     
     return (
         <ThemeProvider theme={darkMode === 'dark' ? darkTheme : lightTheme}>
+            <CssBaseline />
             <Routes>
                 <Route element={<Layout />}>
                     <Route path="/" element={<MainPage />} />
