306e7f75bcaeea5b509341e1b48877348b0b209e
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-03-11 17:59:39 +0900

    fix: api 주소 수정

--- service/src/apis/recommended.ts
+++ service/src/apis/recommended.ts
@@ -8,6 +8,6 @@ export async function recommendedListApi() {
 
 // 포스트 읽음 처리
 export async function readPostApi(id: number) {
-    const res = await HttpClient.get(`api/v1/members/read-post/${id}`);
+    const res = await HttpClient.post(`api/v1/members/me/read-post/${id}`);
     return res;
 }
