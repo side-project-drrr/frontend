526584b3df3eaa7ab656e30459ed2a29558972c6
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-03-23 00:45:42 +0900

    dev: api 연동 완료 알림 기능 테스트 필요, 날짜에 따른 데이터 호출 기능 테스트 필요

--- component/src/stories/header/Alarm.tsx
+++ component/src/stories/header/Alarm.tsx
@@ -8,24 +8,99 @@ import {
     Popper,
     Fade,
     MenuList,
+    Link,
 } from '@mui/material';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import HttpClient from '@monorepo/service/src/apis/HttpClient';
+import { getAuthStorage } from '@monorepo/service/src/repository/AuthRepository';
+import { useTokenDecode } from '@monorepo/service/src/hooks/useTokenDecode';
+import { useNavigate } from 'react-router-dom';
+
+type alarmType = {
+    openStatus: Boolean;
+    readStatus: Boolean;
+    postCount: Number;
+    pushDate: String;
+};
+
+// 날짜 변수 선언
+const currentDate = new Date();
+const sevenDaysAgoDate = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+const today = currentDate.toISOString().slice(0, 10);
+const sevenDaysAgo = sevenDaysAgoDate.toISOString().slice(0, 10);
 
 export const AlarmComponent = () => {
+    const TOKEN_KEY = 'accessToken';
+    const getToken = getAuthStorage(TOKEN_KEY);
+    const memberId = useTokenDecode(getToken);
+    const navigate = useNavigate();
+
     const [open, setOpen] = useState(false);
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const [alarmList, setAlarmList] = useState(false);
-
-    // 알림 여부 가져오는 api
+    const [alarmList, setAlarmList] = useState<alarmType[]>([]);
+    const [mark, setMark] = useState(true);
 
     // 알림 팝업 함수
-    const handleAlarmClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    async function handleAlarmClick(event: React.MouseEvent<HTMLButtonElement>) {
         setAnchorEl(event.currentTarget);
         setOpen(previousOpen => !previousOpen);
 
-        //알림 리스트 가져오는 api 호출
-    };
+        // 알림 열기 처리 api
+        try {
+            const res = await HttpClient.post(`/api/v1/members/me/web-push/posts/open`);
+            console.log(res);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // 알림 리스트 클릭 시
+    async function handleAlarmListClick(date: String) {
+        //알림 읽음 처리 api
+        try {
+            const res = await HttpClient.post(`/api/v1/members/me/web-push/posts/read`, {
+                params: {
+                    pushDate: date,
+                },
+            });
+            console.log(res);
+            if (res.status === 200) {
+                navigate(`/alarm/list?from=${date}&to=${date}`);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        async function getAlarmList() {
+            try {
+                const res = await HttpClient.get(`/api/v1/members/me/web-push/posts/count/7`, {
+                    params: {
+                        memberId: memberId,
+                    },
+                });
+                console.log(res);
+                if (res.status === 200) {
+                    if (res.data.length > 0) {
+                        setAlarmList(res.data);
+
+                        // 가장 최근 날짜를 오픈한 기록이 있다면
+                        if (res.data[0].openStatus) {
+                            setMark(false);
+                        }
+                    } else {
+                        setMark(false);
+                    }
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        memberId && getAlarmList();
+    }, []);
 
     return (
         <>
@@ -35,64 +110,79 @@ export const AlarmComponent = () => {
                 badgeContent={'!'}
                 color="secondary"
                 className="mr-[10px]"
+                invisible={!mark}
             >
                 <NotificationsActiveIcon />
             </Badge>
-            <Popper
-                id="alarm-popper"
-                anchorEl={anchorEl}
-                open={open}
-                placement="bottom-end"
-                modifiers={[
-                    {
-                        name: 'offset',
-                        options: {
-                            offset: [30, 20],
+
+            {mark && (
+                <Popper
+                    id="alarm-popper"
+                    anchorEl={anchorEl}
+                    open={open}
+                    placement="bottom-end"
+                    modifiers={[
+                        {
+                            name: 'offset',
+                            options: {
+                                offset: [30, 20],
+                            },
                         },
-                    },
-                ]}
-                transition
-            >
-                {({ TransitionProps }) => (
-                    <Fade {...TransitionProps} timeout={350}>
-                        <Box
-                            maxHeight={500}
-                            width={350}
-                            overflow="hidden"
-                            bgcolor="background.paper"
-                            p="15px"
-                            border="1px solid primary.main"
-                            borderRadius="20px"
-                            boxShadow={4}
-                        >
-                            <Box width="100%" display="flex" flexDirection="row-reverse" px="15px">
-                                <ButtonBase>
-                                    <Typography variant="body2" py={'10px'}>
-                                        더보기
-                                    </Typography>
-                                </ButtonBase>
+                    ]}
+                    transition
+                >
+                    {({ TransitionProps }) => (
+                        <Fade {...TransitionProps} timeout={350}>
+                            <Box
+                                maxHeight={500}
+                                width={350}
+                                overflow="hidden"
+                                bgcolor="background.paper"
+                                p="15px"
+                                border="1px solid primary.main"
+                                borderRadius="20px"
+                                boxShadow={4}
+                            >
+                                <Box
+                                    width="100%"
+                                    display="flex"
+                                    flexDirection="row-reverse"
+                                    px="15px"
+                                >
+                                    <Link href={`/alarm/list?from=${sevenDaysAgo}&to=${today}`}>
+                                        <ButtonBase>
+                                            <Typography variant="body2" py={'10px'}>
+                                                더보기
+                                            </Typography>
+                                        </ButtonBase>
+                                    </Link>
+                                </Box>
+                                <MenuList>
+                                    {alarmList.map((data, idx) => (
+                                        <>
+                                            <MenuItem
+                                                key={idx}
+                                                onClick={() => handleAlarmListClick(data.pushDate)}
+                                            >
+                                                <Box display="flex" flexDirection="column" py="5px">
+                                                    <Typography variant="body1" mb="2px">
+                                                        ...님이 좋아하실만한 기술 블로그!
+                                                    </Typography>
+                                                    <Typography variant="body2" color="#ABABAB">
+                                                        {data.pushDate}
+                                                        2024.03.10 (금)
+                                                    </Typography>
+                                                </Box>
+                                            </MenuItem>
+                                            <Divider />
+                                        </>
+                                    ))}
+                                </MenuList>
                             </Box>
-                            <MenuList>
-                                {[1, 1, 1, 1, 1].map((data, idx) => (
-                                    <>
-                                        <MenuItem key={idx}>
-                                            <Box display="flex" flexDirection="column" py="5px">
-                                                <Typography variant="body1" mb="2px">
-                                                    ...님이 좋아하실만한 기술 블로그!
-                                                </Typography>
-                                                <Typography variant="body2" color="#ABABAB">
-                                                    2024.03.10 (금)
-                                                </Typography>
-                                            </Box>
-                                        </MenuItem>
-                                        <Divider />
-                                    </>
-                                ))}
-                            </MenuList>
-                        </Box>
-                    </Fade>
-                )}
-            </Popper>
+                        </Fade>
+                    )}
+                </Popper>
+            )}
         </>
     );
 };
--- service/src/pages/AlarmListPage.tsx
+++ service/src/pages/AlarmListPage.tsx
@@ -1,47 +1,30 @@
 import ListboxItem from '@monorepo/component/src/stories/listbox/ListboxItem';
 import { Box, styled } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import dayjs, { Dayjs } from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { useParams } from 'react-router-dom';
+import HttpClient from '../apis/HttpClient';
 
-const item = [
-    {
-        id: 1,
-        techBlogPostBasicInfoDto: {
-            id: 1,
-            title: 'title1',
-            summary: 'summary1',
-            thumbnailUrl: 'thumbnailUrl1',
-            postLike: 1,
-            viewCount: 1,
+type itemType = {
+    id: number;
+    techBlogPostBasicInfoDto: {
+        id: number;
+        title: String;
+        summary: String;
+        thumbnailUrl: String;
+        postLike: number;
+        viewCount: number;
+    };
+    categoryDto: [
+        {
+            id: number;
+            name: String;
         },
-        categoryDto: [
-            {
-                id: 1,
-                name: 'name1',
-            },
-        ],
-    },
-    {
-        id: 2,
-        techBlogPostBasicInfoDto: {
-            id: 2,
-            title: 'title2',
-            summary: 'summary2',
-            thumbnailUrl: 'thumbnailUrl2',
-            postLike: 2,
-            viewCount: 2,
-        },
-        categoryDto: [
-            {
-                id: 2,
-                name: 'name2',
-            },
-        ],
-    },
-];
+    ];
+};
 
 const StyledDatePicker = styled(DatePicker)(({ theme }: { theme: any }) => ({
     '& .MuiInputBase-root': {
@@ -51,8 +34,40 @@ const StyledDatePicker = styled(DatePicker)(({ theme }: { theme: any }) => ({
 }));
 
 export const AlarmListPage = () => {
+    const { from, to } = useParams();
     const [stDate, setStDate] = useState<Dayjs | null>(dayjs(new Date()));
     const [enDate, setEnDate] = useState<Dayjs | null>(dayjs(new Date()));
+    const [list, setList] = useState<itemType[]>([]);
+
+    async function getList(from: string, to: string) {
+        try {
+            const res = await HttpClient.get(`/api/v1/members/me/web-push/posts/date`, {
+                params: {
+                    from: from,
+                    to: to,
+                },
+            });
+
+            console.log(res);
+            if (res.status === 200) {
+                setList(res.data);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        if (from && to) {
+            setStDate(dayjs(from));
+            setEnDate(dayjs(to));
+
+            getList(from, to);
+        } else {
+            console.log(stDate);
+            console.log(enDate);
+        }
+    }, [from, to]);
 
     return (
         <Box>
@@ -63,11 +78,7 @@ export const AlarmListPage = () => {
                     <StyledDatePicker value={enDate} onChange={(date: any) => setEnDate(date)} />
                 </LocalizationProvider>
             </Box>
-            <Box>
-                {item.map(data => (
-                    <ListboxItem key={data.id} item={data} />
-                ))}
-            </Box>
+            <Box>{list && list.map(data => <ListboxItem key={data.id} item={data} />)}</Box>
         </Box>
     );
 };
