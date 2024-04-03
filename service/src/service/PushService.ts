b561d06b7e2211caf026041186f5bf16e0e9a2cf
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-04-03 20:28:36 +0900

    refactor: 리뷰 수정

--- service/src/service/PushService.ts
+++ service/src/service/PushService.ts
@@ -1,37 +1,20 @@
 import axios from 'axios';
 import HttpClient from '../apis/HttpClient';
 
-const BASE_URL = import.meta.env.VITE_APP_BACK_END_LOCAL;
-const token = import.meta.env.VITE_APP_TOKEN;
-
 export async function subscribePush(subscription: any) {
     const jsonString = subscription.toJSON();
-    const res = await axios.post(
-        `${BASE_URL}api/v1/members/me/web-push/subscription`,
-        {
-            endpoint: `${jsonString.endpoint}`,
-            expirationTime: `${jsonString.expirationTime}`,
-            p256dh: `${jsonString.keys.p256dh}`,
-            auth: `${jsonString.keys.auth}`,
-        },
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-        },
-    );
+    const res = await HttpClient.post('api/v1/members/me/web-push/subscription', {
+        endpoint: `${jsonString.endpoint}`,
+        expirationTime: `${jsonString.expirationTime}`,
+        p256dh: `${jsonString.keys.p256dh}`,
+        auth: `${jsonString.keys.auth}`,
+    });
     const data = res.data;
     return data;
 }
 
 export async function unSubscribePush() {
-    const res = await axios.delete(`${BASE_URL}api/v1/members/me/web-push/subscription`, {
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-        },
-    });
+    const res = await axios.delete('api/v1/members/me/web-push/subscription');
     const data = res.data;
     return data;
 }
