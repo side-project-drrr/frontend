51aa8336c695bcae4a9cd453ce644ed8c6c77bf3
Jean Young Park <Jean.Young.Park.96@gmail.com>
2024-04-07 20:56:16 +0900

    feat: view 수정

--- service/src/pages/ViewPage.tsx
+++ service/src/pages/ViewPage.tsx
@@ -1,6 +1,6 @@
-import { Avatar, Box, Button, Chip, Typography, styled } from '@mui/material';
+import { Box, Button, Chip, Typography, styled } from '@mui/material';
 import darkLogo from '@monorepo/service/src/assets/darkLogo.webp';
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getPostApi, readPostApi } from '../apis/view';
 
@@ -16,9 +16,10 @@ type postType = {
     author: String;
     url: String;
 };
+
 export const ViewPage = () => {
     const { postId } = useParams();
-    const [post, setPost] = useState<postType>({});
+    const [post, setPost] = useState<postType>();
     const StyledButton = styled(Button)({
         borderRadius: '15px',
     });
@@ -29,6 +30,10 @@ export const ViewPage = () => {
                 const res = await getPostApi(postId);
 
                 if (res.status === 200) {
+                    const regex = /\./g;
+                    const text = res.data.aiSummary;
+                    const newText = text.replaceAll(regex, '.\r\n');
+                    res.data.aiSummary = newText;
                     setPost(res.data);
                 }
             }
@@ -36,56 +41,65 @@ export const ViewPage = () => {
 
         getPost();
         postId && readPostApi(postId);
-    }, []);
+    }, [postId]);
 
     return (
-        <Box>
-            <Typography variant="h3" textAlign="center">
-                {post.title}
-            </Typography>
-            <Box display="flex" justifyContent="center" alignItems="center" paddingY="25px">
-                <Typography variant="body2">{post.author}</Typography>
-                <span className="px-2">|</span>
-                <Typography variant="body2">{post.writtenDate}</Typography>
-            </Box>
+        post && (
             <Box>
-                {post.thumbnailUrl ? (
-                    <Box bgcolor="background.paper" height="400px" borderRadius="20px">
-                        <img src={`${post.thumbnailUrl}`} alt={`${post.techBlogCode}`} />
-                    </Box>
-                ) : (
+                <Typography variant="h3" textAlign="center">
+                    {post.title}
+                </Typography>
+                <Box display="flex" justifyContent="center" alignItems="center" paddingY="25px">
+                    <Typography variant="body2">{post.author}</Typography>
+                    <span className="px-2">|</span>
+                    <Typography variant="body2">{post.writtenDate}</Typography>
+                </Box>
+                <Box>
                     <Box
                         bgcolor="background.paper"
-                        height="400px"
                         borderRadius="20px"
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center"
-                    >
-                        <img src={darkLogo} alt="logo" />
-                    </Box>
-                )}
+                        margin="0 auto"
+                        sx={{
+                            width: {
+                                xs: '100%',
+                                md: '80%',
+                            },
+                            height: {
+                                xs: '200px',
+                                md: '400px',
+                            },
+                            backgroundImage: `url(${
+                                post.thumbnailUrl ? post.thumbnailUrl : darkLogo
+                            })`,
+                            backgroundRepeat: 'no-repeat',
+                            backgroundPosition: 'center center',
+                            backgroundSize: post.thumbnailUrl ? 'cover' : '50%',
+                        }}
+                    ></Box>
 
-                <Typography variant="body1" paddingY="20px">
-                    {post.aiSummary}
-                </Typography>
-                <Box display="flex" justifyContent="center">
-                    <Chip label="1" />
+                    <Typography variant="body1" paddingY="20px">
+                        {post.aiSummary.split('\r\n').map((line, idx) => (
+                            <Fragment key={idx}>
+                                {line}
+                                <br />
+                            </Fragment>
+                        ))}
+                    </Typography>
+                    <Box display="flex" justifyContent="center">
+                        <Chip label="1" />
+                    </Box>
+                </Box>
+                <Box textAlign="center" marginTop="20px">
+                    <StyledButton
+                        variant="contained"
+                        onClick={() => window.open(`${post.url}`, '_blank')}
+                        rel="noopener noreferrer"
+                        size="large"
+                    >
+                        <Typography variant="h5">블로그로 이동하기</Typography>
+                    </StyledButton>
                 </Box>
             </Box>
-            <Box textAlign="center" marginTop="20px">
-                <StyledButton
-                    variant="contained"
-                    onClick={() => window.open(`${post.url}`, '_blank')}
-                    rel="noopener noreferrer"
-                    size="large"
-                    color="secondary"
-                >
-                    <Typography variant="h5" color="#fff">
-                        블로그로 이동하기
-                    </Typography>
-                </StyledButton>
-            </Box>
-        </Box>
+        )
     );
 };
