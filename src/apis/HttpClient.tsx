3cb967d682b2d0434897ac6b9372401b272d9a9a
rondido <rppr001@naver.com>
2024-05-01 19:10:14 +0900

    ci: yml env production script 실행

--- src/apis/HttpClient.tsx
+++ src/apis/HttpClient.tsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { reissuanceTokenService } from '../service/auth/SocialService';
 import { getAuthStorage, setAccessTokenStorage } from '../repository/AuthRepository';
 
-
 let baseURL = import.meta.env.VITE_APP_API_URL_DEV;
 
 if (process.env.NODE_ENV === 'production') {
