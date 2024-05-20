cf78cb8f7f44afb2c49f0464fc05c93fcfc16ba4
Jean Young Park <jean.young.park.96@gmail.com>
2024-05-20 21:57:35 +0900

    fix: import 수정

--- src/components/card/CardComponent.tsx
+++ src/components/card/CardComponent.tsx
@@ -1,6 +1,5 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { ICardItemsProps } from './type';
 import { Box, Link } from '@mui/material';
