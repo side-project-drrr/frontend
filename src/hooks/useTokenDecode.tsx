9452f461d752c6a366f08e09f7a5c4a82aff39d2
rondido <rppr001@naver.com>
2024-06-04 18:02:17 +0900

    voc-52: 빌드 에러 수정

--- src/hooks/useTokenDecode.tsx
+++
@@ -1,9 +0,0 @@
-import { Base64 } from 'js-base64';
-
-export function useTokenDecode(token: string | null) {
-    if (token !== null) {
-        const payload = token.split('.')[1];
-        const { id } = JSON.parse(Base64.decode(payload));
-        return id;
-    }
-}
