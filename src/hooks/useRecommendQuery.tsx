17fe7023e99dc7e866019f7e0902857529355cb8
rondido <rppr001@naver.com>
2024-06-12 23:04:04 +0900

    review: useQuery hooks에서 http 통신 다시 service로 분리

--- src/hooks/useRecommendQuery.tsx
+++ src/hooks/useRecommendQuery.tsx
@@ -1,17 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
-import HttpClient from '../apis/HttpClient';
 import { useProfileState } from '../context/UserProfile';
-
-export async function getRecommendTechBlogService() {
-    const COUNT = 9;
-
-    try {
-        const res = await HttpClient.get(`/api/v1/members/me/post-recommendation/${COUNT}`);
-        return res.data;
-    } catch (error) {
-        console.error(error);
-    }
-}
+import { getRecommendTechBlogService } from '../service/RecommendService';
 
 export const useRecommendQuery = () => {
     const { token } = useProfileState();
