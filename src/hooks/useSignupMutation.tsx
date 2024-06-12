d3f72fb35d8af0f611aa038e1357fa88a3c7a449
rondido <rppr001@naver.com>
2024-06-12 17:28:50 +0900

    review: Api 기존 service 폴더로 수정

--- src/hooks/useSignupMutation.tsx
+++ src/hooks/useSignupMutation.tsx
@@ -1,33 +1,9 @@
 import { useSetRecoilState } from 'recoil';
-import HttpClient from '../apis/HttpClient';
-import { IAuthProps } from '../service/auth/type';
 import { useMutation } from '@tanstack/react-query';
 import { loginSuccessState } from '../recoil/atom/loginSuccessState';
 import { useProfileState } from '../context/UserProfile';
 import { setAccessTokenStorage, setRefreshTokenStorage } from '../repository/AuthRepository';
-
-export async function SignUpService({
-    email,
-    categoryIds,
-    nickName,
-    provider,
-    providerId,
-    profileImageUrl,
-}: IAuthProps) {
-    try {
-        const res = await HttpClient.post(`/api/v1/auth/signup`, {
-            email: `${email}`,
-            categoryIds: categoryIds.map(Number),
-            nickname: `${nickName}`,
-            provider: `${provider}`,
-            providerId: `${providerId}`,
-            profileImageUrl: `${profileImageUrl}`,
-        });
-        return res.data;
-    } catch (error) {
-        console.error(error);
-    }
-}
+import { SignUpService } from '../service/auth/SocialService';
 
 export const useSignUpMutation = () => {
     const setLoginSucess = useSetRecoilState(loginSuccessState);
