13d20226b19c0b539b5ae0615715242ff79a502f
rondido <rppr001@naver.com>
2024-06-16 22:50:30 +0900

    voc-82: cardList 좋아요 및 조회수 디자인 추가

--- src/components/card/CardComponent.tsx
+++ src/components/card/CardComponent.tsx
@@ -8,15 +8,21 @@ import { useEffect, useState } from 'react';
 import { useProfileState } from '../../context/UserProfile';
 import { useSetRecoilState } from 'recoil';
 import {
+    deletePostLikedService,
     postIncreasedMemberViewsService,
     postIncreasedViewsService,
+    postTechBlogLikeIncreasedService,
 } from '../../service/TechBlogService';
 import darkLogo from '../../assets/darkLogo.webp';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import { techBlogDataState } from '../../recoil/atom/techBlogDataState';
 
 export default function CardComponent({ item, index }: ICardItemsProps) {
     const { token } = useProfileState();
     const [isLogin, setIsLogin] = useState<boolean>(false);
     const setModalOpen = useSetRecoilState(loginModalState);
+    const setTechBlogData = useSetRecoilState(techBlogDataState);
 
     async function postIncreasedViewsRender(postId: string) {
         if (isLogin) {
@@ -25,7 +31,48 @@ export default function CardComponent({ item, index }: ICardItemsProps) {
             postIncreasedViewsService(postId);
         }
     }
+    async function deleteTechBlogLikedRender(postId: number) {
+        await deletePostLikedService(postId);
+    }
 
+    const handlePostLike = async (id: number, e: React.MouseEvent) => {
+        e.stopPropagation();
+        if (!item.hasMemberLikedPost) {
+            await postTechBlogLikeIncreasedService(id);
+            setTechBlogData(prev => {
+                return prev.map(item => {
+                    if (item.techBlogPostBasicInfoDto.id === id) {
+                        return {
+                            ...item,
+                            techBlogPostBasicInfoDto: {
+                                ...item.techBlogPostBasicInfoDto,
+                                likeCount: item.techBlogPostBasicInfoDto.likeCount + 1, // 좋아요 수 증가
+                            },
+                            hasMemberLikedPost: true,
+                        };
+                    }
+                    return item;
+                });
+            });
+        } else {
+            await deleteTechBlogLikedRender(id);
+            setTechBlogData(prev => {
+                return prev.map(item => {
+                    if (item.techBlogPostBasicInfoDto.id === id) {
+                        return {
+                            ...item,
+                            techBlogPostBasicInfoDto: {
+                                ...item.techBlogPostBasicInfoDto,
+                                likeCount: item.techBlogPostBasicInfoDto.likeCount - 1, // 좋아요 수 증가
+                            },
+                            hasMemberLikedPost: false,
+                        };
+                    }
+                    return item;
+                });
+            });
+        }
+    };
     const handleLinkClick = () => {
         if (token === null || token === '') {
             setModalOpen(true);
@@ -40,134 +87,155 @@ export default function CardComponent({ item, index }: ICardItemsProps) {
     return (
         <>
             {!isLogin ? (
-                <Link
-                    href={`/view/${item.techBlogPostBasicInfoDto.id}`}
-                    color="text.primary"
-                    underline="none"
-                    sx={{
+                <Card
+                    sx={(theme: any) => ({
+                        marginTop: '15px',
+                        borderRadius: '20px',
                         '&:hover': {
                             color: 'text.primary',
                         },
                         width: '49%',
-                    }}
-                    onClick={() => postIncreasedViewsRender(item.techBlogPostBasicInfoDto.id)}
+                        [theme.breakpoints.down('sm')]: {
+                            display: 'flex',
+                            flexDirection: 'column',
+                            width: '100%',
+                        },
+                    })}
+                    key={index}
+                    id={item.techBlogPostBasicInfoDto.id}
                 >
-                    <Card
-                        sx={{
-                            marginTop: '15px',
-                            borderRadius: '20px',
-                        }}
-                        key={index}
-                        id={item.techBlogPostBasicInfoDto.id}
-                    >
-                        <div className="px-6 pt-6">
-                            <Box
+                    <div className="px-6 pt-6">
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItem: 'center',
+                                justifyContent: 'center',
+                                width: '100%',
+                                height: '170px',
+                                backgroundColor: 'rgb(158, 158, 158)',
+                                borderRadius: '20px',
+                                backgroundImage: `url(${item.techBlogPostBasicInfoDto.thumbnailUrl ? item.techBlogPostBasicInfoDto.thumbnailUrl : darkLogo})`,
+                                backgroundRepeat: 'no-repeat',
+                                backgroundPosition: 'center center',
+                                backgroundSize: `${item.techBlogPostBasicInfoDto.thumbnailUrl ? '100%' : '10%'}`,
+                            }}
+                        />
+                        <CardContent
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'space-between',
+                                padding: '0',
+                            }}
+                        >
+                            <Link
+                                href={`${item.techBlogPostBasicInfoDto.url}`}
+                                target="_blank"
+                                color="text.primary"
+                                underline="none"
                                 sx={{
-                                    display: 'flex',
-                                    alignItem: 'center',
-                                    justifyContent: 'center',
-                                    width: '100%',
-                                    height: '170px',
-                                    backgroundColor: 'rgb(158, 158, 158)',
-                                    borderRadius: '20px',
-                                    backgroundImage: `url(${item.techBlogPostBasicInfoDto.thumbnailUrl ? item.techBlogPostBasicInfoDto.thumbnailUrl : darkLogo})`,
-                                    backgroundRepeat: 'no-repeat',
-                                    backgroundPosition: 'center center',
-                                    backgroundSize: `${item.techBlogPostBasicInfoDto.thumbnailUrl ? '100%' : '10%'}`,
+                                    '&:hover': {
+                                        color: 'text.primary',
+                                    },
                                 }}
-                            />
-                            <CardContent
+                                onClick={() =>
+                                    postIncreasedViewsRender(item.techBlogPostBasicInfoDto.id)
+                                }
+                            >
+                                <Typography
+                                    gutterBottom
+                                    variant="h5"
+                                    component="div"
+                                    color="text.primary"
+                                    sx={{
+                                        fontSize: '20px',
+                                        width: '100%',
+                                        marginBottom: '1rem',
+                                        textOverflow: 'ellipsis',
+                                        overflow: 'hidden',
+                                        whiteSpace: 'nowrap',
+                                        marginTop: '1.8rem',
+                                    }}
+                                >
+                                    {item.techBlogPostBasicInfoDto.title}
+                                </Typography>
+                            </Link>
+                            <Link
+                                href={`/view/${item.techBlogPostBasicInfoDto.id}`}
+                                color="text.primary"
+                                underline="none"
                                 sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: 'space-between',
-                                    padding: '0',
+                                    '&:hover': {
+                                        color: 'text.primary',
+                                    },
                                 }}
                             >
-                                <Link
-                                    href={`${item.techBlogPostBasicInfoDto.url}`}
-                                    target="_blank"
+                                <Typography
+                                    variant="body2"
                                     color="text.primary"
-                                    underline="none"
                                     sx={{
-                                        '&:hover': {
-                                            color: 'text.primary',
-                                        },
-                                    }}
-                                    onClick={() =>
-                                        postIncreasedViewsRender(item.techBlogPostBasicInfoDto.id)
-                                    }
-                                >
-                                    <Typography
-                                        gutterBottom
-                                        variant="h5"
-                                        component="div"
-                                        color="text.primary"
-                                        sx={{
-                                            fontSize: '20px',
-                                            width: '100%',
-                                            marginBottom: '1rem',
-                                            textOverflow: 'ellipsis',
-                                            overflow: 'hidden',
-                                            whiteSpace: 'nowrap',
-                                            marginTop: '1.8rem',
-                                        }}
-                                    >
-                                        {item.techBlogPostBasicInfoDto.title}
-                                    </Typography>
-                                </Link>
-                                <Link
-                                    href={`/view/${item.techBlogPostBasicInfoDto.id}`}
-                                    color="text.primary"
-                                    underline="none"
-                                    sx={{
-                                        '&:hover': {
-                                            color: 'text.primary',
-                                        },
+                                        width: '100%',
+                                        display: 'block',
+                                        overflow: 'hidden',
+                                        height: '6.5rem',
+                                        marginBottom: '1.5rem',
+                                        lineHeight: '1.25',
                                     }}
                                 >
-                                    <Typography
-                                        variant="body2"
-                                        color="text.primary"
-                                        sx={{
-                                            width: '100%',
-                                            display: 'block',
-                                            overflow: 'hidden',
-                                            height: '6.5rem',
-                                            marginBottom: '1.5rem',
-                                            lineHeight: '1.25',
-                                        }}
-                                    >
-                                        {item.techBlogPostBasicInfoDto.summary}
-                                    </Typography>
-                                    <Typography
-                                        variant="body2"
-                                        color="text.primary"
-                                        sx={{
-                                            paddingBottom: '1rem',
-                                            textOverflow: 'ellipsis',
-                                            whiteSpace: 'nowarp',
-                                            lineHeight: '1.25',
-                                        }}
-                                    >
-                                        <ul className="flex flex-wrap w-full overflow-y-hidden h-[30px] ">
-                                            {item.categoryDto?.map((item: any) => (
-                                                <li
-                                                    key={item.id}
-                                                    id={item.id}
-                                                    className="mr-2 text-xs"
-                                                >
-                                                    #{item.name}
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary"></Typography>
-                                </Link>
-                            </CardContent>
-                        </div>
-                    </Card>
-                </Link>
+                                    {item.techBlogPostBasicInfoDto.summary}
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    color="text.primary"
+                                    sx={{
+                                        paddingBottom: '1rem',
+                                        textOverflow: 'ellipsis',
+                                        whiteSpace: 'nowarp',
+                                        lineHeight: '1.25',
+                                    }}
+                                >
+                                    <span className="flex flex-wrap w-full overflow-y-hidden h-[30px] ">
+                                        {item.categoryDto?.map((item: any) => (
+                                            <span
+                                                key={item.id}
+                                                id={item.id}
+                                                className="mr-2 text-xs"
+                                            >
+                                                #{item.name}
+                                            </span>
+                                        ))}
+                                    </span>
+                                </Typography>
+                            </Link>
+                            <span
+                                className="flex justify-between w-3/12 mt-[10px] items-center"
+                                onClick={e => e.stopPropagation()}
+                            >
+                                <span className="flex items-center justify-center text-xs gap-1">
+                                    <ThumbUpIcon
+                                        fontSize="small"
+                                        onClick={e =>
+                                            handlePostLike(
+                                                Number(item.techBlogPostBasicInfoDto.id),
+                                                e,
+                                            )
+                                        }
+                                        sx={
+                                            item.hasMemberLikedPost
+                                                ? { color: '#E6783A', fontSize: '18px' }
+                                                : { color: '', fontSize: '18px' }
+                                        }
+                                    />
+                                    {item.techBlogPostBasicInfoDto.likeCount}
+                                </span>
+                                <span className="text-xs flex items-center gap-1">
+                                    <RemoveRedEyeIcon />
+                                    {item.techBlogPostBasicInfoDto.viewCount}
+                                </span>
+                            </span>
+                        </CardContent>
+                    </div>
+                </Card>
             ) : (
                 <Link
                     color="text.primary"
@@ -253,15 +321,30 @@ export default function CardComponent({ item, index }: ICardItemsProps) {
                                         lineHeight: '1.25',
                                     }}
                                 >
-                                    <ul className="flex flex-wrap w-full overflow-y-hidden h-[30px] ">
+                                    <span className="flex flex-wrap w-full overflow-y-hidden h-[30px] ">
                                         {item.categoryDto?.map((item: any) => (
-                                            <li key={item.id} id={item.id} className="mr-2 text-xs">
+                                            <span
+                                                key={item.id}
+                                                id={item.id}
+                                                className="mr-2 text-xs"
+                                            >
                                                 #{item.name}
-                                            </li>
+                                            </span>
                                         ))}
-                                    </ul>
+                                    </span>
                                 </Typography>
-                                <Typography variant="body2" color="text.secondary"></Typography>
+                                <Typography variant="body2" color="text.primary">
+                                    <span className="flex justify-between w-3/12 mt-[10px] items-center ">
+                                        <span className="flex items-center justify-center text-xs gap-1">
+                                            <ThumbUpIcon fontSize="small" />
+                                            {item.techBlogPostBasicInfoDto.likeCount}
+                                        </span>
+                                        <span className="text-xs flex items-center gap-1">
+                                            <RemoveRedEyeIcon />
+                                            {item.techBlogPostBasicInfoDto.viewCount}
+                                        </span>
+                                    </span>
+                                </Typography>
                             </CardContent>
                         </div>
                     </Card>
